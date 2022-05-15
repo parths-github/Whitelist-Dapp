57bcc90508bc8a6607c6f7513c5c9ce679c44823
parths_github <parth601shah@gmail.com>
2022-05-16 00:09:57 +0530

    Adding all files

---
+++ hardhat-tutorial/scripts/deploy.js
@@ -0,0 +1,18 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+    const whitelistContract = await ethers.getContractFactory("Whitelist");
+    const WhitelistContract = await whitelistContract.deploy(10);
+
+    await WhitelistContract.deployed();
+
+    console.log(`WhitelistContract deployed to ${WhitelistContract.address}`);
+
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    })
