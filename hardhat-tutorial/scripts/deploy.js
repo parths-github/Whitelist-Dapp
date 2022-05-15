1429a464670702e7ca3c4164bab805764e9bc10b
parths_github <parth601shah@gmail.com>
2022-05-16 01:02:28 +0530

    Bug fixing

--- hardhat-tutorial/scripts/deploy.js
+++ hardhat-tutorial/scripts/deploy.js
@@ -3,7 +3,7 @@ const { ethers } = require("hardhat");
 async function main() {
     const whitelistContract = await ethers.getContractFactory("Whitelist");
     const WhitelistContract = await whitelistContract.deploy(10);
-
+    console.log("Deploying...");
     await WhitelistContract.deployed();
 
     console.log(`WhitelistContract deployed to ${WhitelistContract.address}`);
--- my-app/constants/index.js
+++ my-app/constants/index.js
@@ -1,66 +1,66 @@
 export const abi =  [
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "_maxWhitelistedAddresses",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "nonpayable",
-      "type": "constructor"
-    },
-    {
-      "inputs": [],
-      "name": "addAddressToWhitelist",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "maxWhitelistedAddresses",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "numAddressesWhitelisted",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "",
-          "type": "address"
-        }
-      ],
-      "name": "whitelistedAddresses",
-      "outputs": [
-        {
-          "internalType": "bool",
-          "name": "",
-          "type": "bool"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    }
-  ];
-export const WHITELIST_CONTRACT_ADDRESS = "0x6381E8B9922a367Fadb5C5D2aEBB1308cef699a5";
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_maxWhitelistedAddresses",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [],
+    "name": "addAddressToWhitelist",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "maxWhitelistedAddresses",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "numAddressesWhitelisted",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "whitelistedAddresses",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
+export const WHITELIST_CONTRACT_ADDRESS = "0x7095Bf6A5feFb71D182c275bC0b086e3a22d8022";
